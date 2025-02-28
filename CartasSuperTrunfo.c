b3b98075c55a413e9f21139c05b4de8f4040b162
OtatsuMG <86794959+OtatsuMG@users.noreply.github.com>
2025-02-28 16:41:50 +0000

    NIVEL AVENTUREIRO: + DENSIDADE, PIB PER CAPITA;

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,6 +4,7 @@ int main() {
     printf("\nDesafio Super Trunfo!\n"); // TÍTULO DO JOGO
 
     // VARIÁVEIS DAS CARTAS
+    //***NIVEL NOVATO***
     char estado1, estado2; // NOME DOS ESTADOS DAS CARTAS
     char codigo1[5], codigo2[5]; // CÓDIGO DAS CARTAS 
     char cidade1[50], cidade2[50]; // NOME DAS CIDADES 
@@ -12,7 +13,15 @@ int main() {
     float pib1, pib2; // PIB (EM BILHÕES DE REAIS)
     int pontos_turisticos1, pontos_turisticos2; // QUANTIDADE DE PONTOS TURÍSTICOS
 
+    //***NIVEL AVENTUREIRO***
+    float densidadepopulacional1;
+    float pibpercapita1;
+
+    float densidadepopulacional2;
+    float pibpercapita2;
+
     // FORNECIMENTO DAS INFORMAÇÕES DA CARTA 1
+    //NIVEL NOVATO
     printf("\nDados da Carta 1\n");
 
     printf("Estado (letra de A a H):");
@@ -36,7 +45,9 @@ int main() {
     printf("Número de Pontos Turísticos:");
     scanf("%d", &pontos_turisticos1);
 
+
     // FORNECIMENTO DAS INFORMAÇÕES DA CARTA 2
+    //NIVEL NOVATO
     printf("\nDados da Carta 2\n");
 
     printf("Estado (letra de A a H):");
@@ -60,7 +71,15 @@ int main() {
     printf("Número de Pontos Turísticos:");
     scanf("%d", &pontos_turisticos2);
 
+    //***NIVEL AVENTUREIRO***
+    densidadepopulacional1 = (float)populacao1 / area1;
+    pibpercapita1 = (float)(pib1 * 10000000000) / (populacao1 * 10);
+
+    densidadepopulacional2 = (float)populacao2 / area2;
+    pibpercapita2 = (float)(pib2 * 10000000000) / (populacao2 * 10);
+
     // EXIBIÇÃO DA CARTA 1
+    //NIVEL NOVATO
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -70,7 +89,13 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
 
+    //NIVEL AVENTUREIRO
+    printf("Densidade populacional: %.2f hab/km²\n", densidadepopulacional1);
+    printf("Pib per capita: %.2f reais\n", pibpercapita1);
+    //DENSIDADE POPULACIONAL E PIB PER CAPITA
+
     // EXIBIÇÃO DA CARTA 2
+    //NIVEL NOVATO
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2); 
     printf("Código: %s\n", codigo2);
@@ -79,6 +104,11 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+
+    //NIVEL AVENTUREIRO
+    printf("Densidade populacional: %.2f hab/km²\n", densidadepopulacional2);
+    printf("Pib per capita: %.2f reais\n", pibpercapita2);
+    //DENSIDADE POPULACIONAL E PIB PER CAPITA
 
     return 0;
 }
