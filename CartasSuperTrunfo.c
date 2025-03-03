997f797829e753a585bdfd6e8d0b0913b067fa7e
OtatsuMG <86794959+OtatsuMG@users.noreply.github.com>
2025-03-03 15:15:39 +0000

    Desafio Super Trunfo - Nivel Mestre - Comparação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,14 +14,14 @@ int main() {
     int pontos_turisticos1, pontos_turisticos2; // QUANTIDADE DE PONTOS TURÍSTICOS
 
     //***NIVEL AVENTUREIRO***
-    float densidadepopulacional1;
-    float pibpercapita1;
+    float densidadepopulacional1, densidadepopulacional2;
+    float pibpercapita1, pibpercapita2;
 
-    float densidadepopulacional2;
-    float pibpercapita2;
+    //***NIVEL MESTRE***
+    float SuperPoderA, SuperPoderB;
 
     // FORNECIMENTO DAS INFORMAÇÕES DA CARTA 1
-    //NIVEL NOVATO
+    //***NIVEL NOVATO***
     printf("\nDados da Carta 1\n");
 
     printf("Estado (letra de A a H):");
@@ -45,9 +45,8 @@ int main() {
     printf("Número de Pontos Turísticos:");
     scanf("%d", &pontos_turisticos1);
 
-
     // FORNECIMENTO DAS INFORMAÇÕES DA CARTA 2
-    //NIVEL NOVATO
+    //***NIVEL NOVATO***
     printf("\nDados da Carta 2\n");
 
     printf("Estado (letra de A a H):");
@@ -78,8 +77,12 @@ int main() {
     densidadepopulacional2 = (float)populacao2 / area2;
     pibpercapita2 = (float)(pib2 * 10000000000) / (populacao2 * 10);
 
+    //***NIVEL MESTRE*** //SOMA DOS ATRIBUTOS DAS CARTAS
+    SuperPoderA = (float) populacao1 + area1 + pib1 + pontos_turisticos1 + pibpercapita1 + (1 / densidadepopulacional1);
+    SuperPoderB = (float) populacao2 + area2 + pib2 + pontos_turisticos2 + pibpercapita2 + (1 / densidadepopulacional2);
+
     // EXIBIÇÃO DA CARTA 1
-    //NIVEL NOVATO
+    //***NIVEL NOVATO***
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -89,13 +92,12 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
 
-    //NIVEL AVENTUREIRO
+    //***NIVEL AVENTUREIRO*** //DENSIDADE POPULACIONAL E PIB PER CAPITA
     printf("Densidade populacional: %.2f hab/km²\n", densidadepopulacional1);
     printf("Pib per capita: %.2f reais\n", pibpercapita1);
-    //DENSIDADE POPULACIONAL E PIB PER CAPITA
 
     // EXIBIÇÃO DA CARTA 2
-    //NIVEL NOVATO
+    //***NIVEL NOVATO*** //DENSIDADE POPULACIONAL E PIB PER CAPITA
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2); 
     printf("Código: %s\n", codigo2);
@@ -105,10 +107,24 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
 
-    //NIVEL AVENTUREIRO
+    //***NIVEL AVENTUREIRO***
     printf("Densidade populacional: %.2f hab/km²\n", densidadepopulacional2);
     printf("Pib per capita: %.2f reais\n", pibpercapita2);
     //DENSIDADE POPULACIONAL E PIB PER CAPITA
+
+      //***NIVEL MESTRE*** //COMPARAÇÃO DAS CARTAS
+      printf("\nResultado do Duelo:\n");
+      printf("População: Carta 1 venceu (%d)\n", populacao1 > populacao2);
+      printf("Área: Carta 1 venceu (%d)\n", area1 > area2);
+      printf("PIB: Carta 1 venceu (%d)\n", pib1 > pib2);
+      printf("Pontos Turísticos: Carta 1 venceu (%d)\n", pontos_turisticos1 > pontos_turisticos2);
+      printf("Densidade Populacional: Carta 1 venceu (%d)\n", densidadepopulacional1 < densidadepopulacional2);
+      printf("PIB per capita: Carta 1 venceu (%d)\n", pibpercapita1 > pibpercapita2);
+      printf("Super Poder: Carta 1 venceu (%d)\n", SuperPoderA > SuperPoderB);
+  
+      //APENAS PARA SABER O VALOR DO SUPERPODER INDIVIDUAL
+      printf("\nSuperPoderA: %.2f\n", SuperPoderA);
+      printf("SuperPoderB: %.2f\n", SuperPoderB);  
 
     return 0;
 }
